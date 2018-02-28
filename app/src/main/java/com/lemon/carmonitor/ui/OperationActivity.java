7471835a59bf36428e68f6a25d4aa78676c50b7a
xflu <454162034@qq.com>
2018-02-28 16:10:53 +0800

    all ui activity and fragment

---
+++ app/src/main/java/com/lemon/carmonitor/ui/OperationActivity.java
@@ -0,0 +1,17 @@
+package com.lemon.carmonitor.ui;
+
+/**
+ * 项目名称:  [CarMagic]
+ * 包:        [com.lemon.carmonitor.ui]
+ * 类描述:    [类描述]
+ * 创建人:    [xflu]
+ * 创建时间:  [2018/2/28 15:49]
+ * 修改人:    [xflu]
+ * 修改时间:  [2018/2/28 15:49]
+ * 修改备注:  [说明本次修改内容]
+ * 版本:      [v1.0]
+ */
+
+
+public class OperationActivity {
+}
